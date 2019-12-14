ff3935fa6ee8fa23de012336d5899dbc50163387
Prasad <kah3679@aero.local>
2019-12-14 13:07:51 -0700

    Prasad-1234: Testing my comments commit

--- src/main/java/com/group/flywaydemo/FlywaydemoApplication.java
+++ src/main/java/com/group/flywaydemo/FlywaydemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlywaydemoApplication {
-
+	/**
+	* This is a test comment
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(FlywaydemoApplication.class, args);
 	}
